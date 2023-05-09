27f34749f0e0356a9a7471df93f40d69cd38f14d
Jaswanth03-cyber <jaswanth.varada26@gmail.com>
2023-05-09 19:28:52 +0530

    added db.js

---
+++ backend/db.js
@@ -0,0 +1,11 @@
+require("dotenv").config({path:'./config.txt'}) ;
+
+const pool = new Pool({
+    user: process.env.USER,
+    host: process.env.HOST,
+    database: process.env.DATABASE,
+    password: process.env.PASSWORD,
+    port: process.env.DBPORT,
+});
+
+module.exports = pool;
