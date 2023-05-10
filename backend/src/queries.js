7cce308cf5a97fbe81ee38ebd091cbe32988772b
Jaswanth03-cyber <jaswanth.varada26@gmail.com>
2023-05-10 07:46:41 +0530

    added a query

---
+++ backend/src/queries.js
@@ -0,0 +1,12 @@
+// venue related queries
+const venuedetails = "SELECT * FROM venue" ;
+
+
+//player statistics related queries
+
+
+// match summary related queries
+
+module.exports = {
+    venuedetails,
+};
